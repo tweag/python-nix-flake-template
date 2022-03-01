ad7d0cfe225ebdc06ce6875b6712f9c817cb132e
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2022-03-01 10:28:11 +0100

    Fix path to template

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
   outputs = { self, ... }:
     {
-      defaultTemplate = {
-        description = "A Python development environment using venv that includes pyspark with JDK provided by Nix";
-        path = "pyspark";
+      templates = {
+        python-env = {
+          description = "A Python development environment using venv that includes pyspark with JDK provided by Nix";
+          path = "python-env";
+        };
       };
+
+      defaultTemplate = self.templates.python-env;
     };
 }
